a785b6a43747af681036efcd6a06f270198d52d2
Matthew McGrattan <matthew.mcgrattan@bodleian.ox.ac.uk>
2020-09-07 22:09:50 +0100

    Added weighting and override the save method on the IIIF model

--- search_service/search/models.py
+++ search_service/search/models.py
@@ -3,7 +3,7 @@ import uuid
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.postgres.search import SearchVectorField
+from django.contrib.postgres.search import SearchVectorField, SearchVector
 from django.contrib.postgres.indexes import GinIndex
 from django.db import models
 from model_utils.models import TimeStampedModel
@@ -79,6 +79,14 @@ class PresentationAPIResource(TimeStampedModel):
     )
     navdate = models.DateTimeField(blank=True, null=True, verbose_name=_("Navigation date"))
     search_vect = SearchVectorField(null=True)
+
+    def save(self, *args, **kwargs):
+        self.search_vector = (
+                SearchVector('label', weight='A')
+                + SearchVector('description', weight='C')
+                + SearchVector('attribution', weight='A')
+        )
+        super().save(*args, **kwargs)
 
     class Meta:
         indexes = [
