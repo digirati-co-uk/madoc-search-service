c26a0173ae1c22a95a2513e72183b839ad6ef010
Matt McGrattan <matt.mcgrattan@digirati.com>
2020-09-10 21:45:02 +0100

    Remove unused signals

--- search_service/search/signals.py
+++ search_service/search/signals.py
@@ -1,17 +1,16 @@
-from .models import PresentationAPIResource
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.postgres.search import SearchVectorField, SearchVector
 
 
-@receiver(post_save, sender=PresentationAPIResource)
-def update_searchvector(sender, instance, **kwargs):
-    print("I updated the vectors")
-    instance.search_vect = (
-            SearchVector("label", weight="A")
-            + SearchVector("description", weight="C")
-            + SearchVector("attribution", weight="A")
-            + SearchVector("m_summary", weight="B",
-                           )
-    )
-    instance.save(update_fields=['search_vect'])
+# @receiver(post_save, sender=PresentationAPIResource)
+# def update_searchvector(sender, instance, **kwargs):
+#     print("I updated the vectors")
+#     instance.search_vect = (
+#             SearchVector("label", weight="A")
+#             + SearchVector("description", weight="C")
+#             + SearchVector("attribution", weight="A")
+#             + SearchVector("m_summary", weight="B",
+#                            )
+#     )
+#     instance.save(update_fields=['search_vect'])
