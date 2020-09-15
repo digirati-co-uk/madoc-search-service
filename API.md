81e861173a3642d5c3dc586b24a33f9d4b14a775
Matt McGrattan <matt.mcgrattan@digirati.com>
2020-09-15 15:06:21 +0100

    Quick sketch of API updated Comments on tests.py Docstring change on views.py

--- API.md
+++ API.md
@@ -36,11 +36,17 @@ The payload format is:
 
 ## Query API
 
-The basic query parameters are as follows:
+The search API is at `search/`. 
+
+The basic query parameters are as follows
+
 
 * `?fulltext=foo` : search against the preindexed fulltext (term vectors) for `foo`
 * `?contexts__id=urn:madoc:site:2`: filter to just objects with that context (N.B. that's a double underscore)
 * `?madoc_id=urn:madoc:manifest:saa-AA0428a`: filter to just hits within that one object (n.b. that's a single underscore)
+
+In principle, we can add filters trivially easy for any other fields in the IIIF resource that make sense to filter on. 
+They need to be added to a list of filterset_fields, but other than that, the code is already in place.
 
 Querying for a facet:
 
--- search_service/search/tests.py
+++ search_service/search/tests.py
@@ -1,23 +1,20 @@
 import requests
 
 collections = [
-    # "https://iiif.ub.uni-leipzig.de/static/collections/Drucke17/collection.json",
-    # "https://iiif.ub.uni-leipzig.de/static/collections/Drucke16/collection.json",
-    # "https://iiif.ub.uni-leipzig.de/static/collections/misc/cdvost2018.json",
-    # "https://iiif.hab.de/collection/project/mssox.json",
-    # "https://www.e-codices.unifr.ch/metadata/iiif/collection/sl.json",
-    # "https://www.e-codices.unifr.ch/metadata/iiif/collection/saa.json",
-    # "https://www.e-codices.unifr.ch/metadata/iiif/collection/bge.json",
-    # "https://digital.library.villanova.edu/Collection/vudl:294849/IIIF",
-    # "https://digital.library.villanova.edu/Collection/vudl:289364/IIIF",
-    # "https://digital.library.villanova.edu/Collection/vudl:313874/IIIF",
-    # "https://digital.library.villanova.edu/Collection/vudl:293828/IIIF",
-    # "https://digital.library.villanova.edu/Collection/vudl:287996/IIIF",
-    # "https://digital.library.villanova.edu/Collection/vudl:321794/IIIF",
-    # "https://digital.library.villanova.edu/Collection/vudl:289113/IIIF",
-    # "https://iiif.bodleian.ox.ac.uk/iiif/collection/korean-origin",
-    # "https://iiif.bodleian.ox.ac.uk/iiif/collection/japanese-origin",
-    # "https://iiif.bodleian.ox.ac.uk/iiif/collection/southeast-asian-origin",
+    "https://iiif.ub.uni-leipzig.de/static/collections/Drucke17/collection.json",
+    "https://iiif.ub.uni-leipzig.de/static/collections/Drucke16/collection.json",
+    "https://iiif.ub.uni-leipzig.de/static/collections/misc/cdvost2018.json",
+    "https://iiif.hab.de/collection/project/mssox.json",
+    "https://www.e-codices.unifr.ch/metadata/iiif/collection/sl.json",
+    "https://www.e-codices.unifr.ch/metadata/iiif/collection/saa.json",
+    "https://www.e-codices.unifr.ch/metadata/iiif/collection/bge.json",
+    "https://digital.library.villanova.edu/Collection/vudl:294849/IIIF",
+    "https://digital.library.villanova.edu/Collection/vudl:289364/IIIF",
+    "https://digital.library.villanova.edu/Collection/vudl:313874/IIIF",
+    "https://digital.library.villanova.edu/Collection/vudl:293828/IIIF",
+    "https://digital.library.villanova.edu/Collection/vudl:287996/IIIF",
+    "https://digital.library.villanova.edu/Collection/vudl:321794/IIIF",
+    "https://digital.library.villanova.edu/Collection/vudl:289113/IIIF",
     "https://view.nls.uk/collections/7446/74466699.json"
 ]
 
@@ -32,16 +29,16 @@ for coll in collections:
         j = requests.get(m["@id"]).json()
         if j:
             post_json = {
-                "contexts": [
+                "contexts": [  # List of contexts with their id and type
                     {"id": "urn:madoc:site:2", "type": "Site"},
                     {
                         "id": coll,
                         "type": "Collection",
                     },
                 ],
-                "resource": j,
-                "id": f"urn:madoc:manifest:{m['@id'].split('/')[-2]}",
-                "thumbnail": f"http://madoc.foo/thumbnail/{m['@id'].split('/')[-2]}/fake.jpg",
+                "resource": j,  # this is the JSON for the IIIF resource
+                "id": f"urn:madoc:manifest:{m['@id'].split('/')[-2]}",  # Madoc ID for the subject/object
+                "thumbnail": f"http://madoc.foo/thumbnail/{m['@id'].split('/')[-2]}/fake.jpg",  # Thumbnail URL
             }
             headers = {"Content-Type": "application/json", "Accept": "application/json"}
             p = requests.post(url="http://localhost:8000/iiif/", json=post_json, headers=headers)
