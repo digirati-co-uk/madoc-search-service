cca896ee5107a0a0f3cff19540af133cf63eef2c
Matt McGrattan <matt.mcgrattan@digirati.com>
2021-05-11 11:58:38 +0100

    Page size on pagination class

--- search_service/search/pagination.py
+++ search_service/search/pagination.py
@@ -1,4 +1,5 @@
 import logging
+from django.conf import settings
 
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
@@ -61,12 +62,15 @@ class MadocPagination(PageNumberPagination):
         "totalResults": 830
       }
     """
+    page_size_query_param = "page_size"
+    max_page_size = settings.MAX_PAGE_SIZE
 
     def get_paginated_response(self, data):
         return Response(
             {
                 "pagination": {
                     "page": self.page.number,
+                    "pageSize": self.page.paginator.per_page,
                     "next": self.get_next_link(),
                     "previous": self.get_previous_link(),
                     "totalPages": self.page.paginator.num_pages,
